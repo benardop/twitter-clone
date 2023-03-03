bc01441169d6477752318bc9e8b29f2b251a808f
benardop <benard.oc.p@gmail.com>
2023-03-03 18:22:18 +0300

    Make a basic user model(including password authentication)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "benard", email: "benard@gmail.com")
+    @user = User.new(name: "benard", email: "benard@gmail.com", password: "foobar",
+                      password_confirmation: "foobar")
   end
     test "should be valid" do
       assert @user.valid?
@@ -49,5 +50,22 @@ class UserTest < ActiveSupport::TestCase
       duplicate_user = @user.dup
       @user.save
       assert_not duplicate_user.valid?
+    end
+
+    test "user email should be saved as lowercase" do
+      mixed_case_email = "Foo@Example.Com"
+      @user.email = mixed_case_email
+      @user.save
+      assert_equal mixed_case_email.downcase, @user.reload.email
+    end
+
+    test "password should be presnt(non blank)" do
+       @user.password = @user.password_confirmation = " " * 6
+       assert_not @user.valid?
+    end
+
+    test "password should have a minimum length" do
+       @user.password = @user.password_confirmation = "a" * 5
+       assert_not @user.valid?
     end
 end
