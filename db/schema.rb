bc49046f78acf2dca75d17e43a5cea3c02579487
benardop <benard.oc.p@gmail.com>
2023-03-14 00:55:43 +0300

    Add integration tests for successful user signup

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_081040) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.index ["email"], name: "index_users_on_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
