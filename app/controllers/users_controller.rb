5120ec9fa0bf2e91534f7b6da762bfae11e08387
benardop <benard.oc.p@gmail.com>
2023-03-14 00:49:59 +0300

    Add flash message displayed when a new user is created and saved successful

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save
-      #handle successful save
+      flash[:success] = "Welcome to twitter clone blog application!"
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
