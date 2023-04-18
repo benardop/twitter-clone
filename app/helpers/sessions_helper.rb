aca955cfa4054eff8a9de5094dfaf986d072a07d
benardop <benard.oc.p@gmail.com>
2023-04-18 23:39:25 +0300

    Add the code to return the current logged-in user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,14 @@
 module SessionsHelper
+
+  # logs in the given user
   def log_in(user_id)
     session[:user_id] = user_id
+  end
+
+  # Return the current loggen-in user (if any)
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
   end
 end
