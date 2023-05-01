660f7a2355cf181d8c886c50a0b0fac8c2bfafc9
benardop <benard.oc.p@gmail.com>
2023-05-02 01:09:13 +0300

    Implement basic login functionality in the app

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,28 +1,45 @@
 require "test_helper"
 
-class UsersLoginTest < ActionDispatch::IntegrationTest
+class UsersLogin < ActionDispatch::IntegrationTest
 
   def setup 
     @user = users(:benard)
   end
+end
 
-  test "login with valid email/invalid password" do
+class InvalidPasswordTest < UsersLogin 
+  test "login path" do 
     get login_path
     assert_template 'sessions/new'
+  end
 
-    post login_path params: { session: { email: "benard@example.com", password: "invalid" }}
+  test "login with valid email/invalid password" do    
 
-    assert_response :unprocessable_entity
+    post login_path params: { session: { email: @user.email, password: "invalid" }}
+
+    assert_not is_logged_in?
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
   end
-
-  test "login with valid information" do
+end
+  
+class ValidLogin < UsersLogin 
+  def setup 
+    super
     post login_path params: { session: { email: @user.email, password: 'password' }}
+  end
+end
 
-    assert_redirected_to @user
+class ValidLoginTest < ValidLogin
+
+  test "valid login" do 
+    assert is_logged_in?
+    assert assert_redirected_to @user
+  end
+
+  test "redirect after login" do 
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
@@ -30,3 +47,25 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user)
   end
 end
+
+class Logout < ValidLogin
+  def setup
+    super
+    delete logout_path
+  end
+end
+  
+  class LogoutTest < Logout
+    test "successful logout" do 
+      assert_not is_logged_in?
+      assert_response :see_other
+      assert_redirected_to root_url
+    end
+
+    test "redirect after logout" do 
+      follow_redirect!
+      assert_select "a[href=?]", login_path
+      assert_select "a[href=?]", logout_path, count: 0
+      assert_select "a[href=?]", user_path(@user), count: 0
+    end
+  end
