9ed8d6b434485e974855986424cc00e807ac8477
benardop <benard.oc.p@gmail.com>
2023-05-22 10:39:54 +0300

    Add user login authorization steps before porfile  edit and update

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,6 +7,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "test unseccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user, params: {user: {name: "",
@@ -18,6 +19,8 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     name = "benard"
