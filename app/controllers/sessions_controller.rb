5d22460f173f7261297f434e0a046687a0f458ef
benardop <benard.oc.p@gmail.com>
2023-05-24 13:50:07 +0300

    Add user index view page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
 
-  def new
-    debugger
-  end
+  # def new
+  #   debugger
+  # end
   
   def create 
     user = User.find_by(email: params[:session][:email].downcase)
